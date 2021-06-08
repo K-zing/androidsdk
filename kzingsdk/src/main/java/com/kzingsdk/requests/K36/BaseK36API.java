16a76b94498077290710c33eaf5b243937d98ffb
Wing <wing.lee@lokchong.com>
2021-06-08 14:28:29 +0800

    3.42

--- kzingsdk/src/main/java/com/kzingsdk/requests/K36/BaseK36API.java
+++ kzingsdk/src/main/java/com/kzingsdk/requests/K36/BaseK36API.java
@@ -1,6 +1,13 @@
 package com.kzingsdk.requests.K36;
 
+import android.content.Context;
+
 import com.kzingsdk.core.CoreRequest;
+import com.kzingsdk.entity.K36.TransferBonusActivity;
+
+import org.json.JSONObject;
+
+import io.reactivex.Observable;
 
 public abstract class BaseK36API extends CoreRequest {
 
@@ -11,6 +18,12 @@ public abstract class BaseK36API extends CoreRequest {
     @Override
     protected String getAction() {
         return Action.baseAction;
+    }
+
+    @Override
+    public Observable<JSONObject> baseExecute(Context context) {
+        return super.baseExecute(context)
+                .map(jsonResponse -> jsonResponse.optJSONObject("data"));
     }
 
 }
